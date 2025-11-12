9d23aa52d5008f500aff89b4b51c1268eb95db54
Jhan24b <jhan24b@gmail.com>
2025-11-11 22:05:11 -0500

    path v1

--- app/api/colective/path/[driverId]/route.ts
+++ app/api/colective/path/[driverId]/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: NextRequest) {
       if (result.rows.length > 0) {
         return NextResponse.json(
           {
-            result: result.rows[0],
+            result: result.rows[0]
           },
           { status: 201 }
         );
@@ -37,29 +37,29 @@ export async function POST(req: NextRequest) {
   }
 }
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { driverId: string } }
-) {
-  const { driverId } = params;
+// export async function GET(
+//   req: NextRequest,
+//   { params }: { params: { driverId: string } }
+// ) {
+//   const { driverId } = params;
 
-  const routes = [
-    {
-      route_id: 12,
-      route_name: "Centro Histórico",
-      driver_name: "Carlos Vega",
-      driver_photo: "https://cdn/img/driver12.jpg",
-      desvio: false,
-      coordinates: [
-        { lat: -12.046, lng: -77.042, timestamp: "2025-11-10T12:00:00Z" },
-        { lat: -12.047, lng: -77.041, timestamp: "2025-11-10T12:01:00Z" },
-        { lat: -12.048, lng: -77.04, timestamp: "2025-11-10T12:02:00Z" },
-        { lat: -12.049, lng: -77.039, timestamp: "2025-11-10T12:03:00Z" },
-        { lat: -12.05, lng: -77.038, timestamp: "2025-11-10T12:04:00Z" },
-      ],
-      fecha: new Date().toISOString(),
-    },
-  ];
+//   const routes = [
+//     {
+//       route_id: 12,
+//       route_name: "Centro Histórico",
+//       driver_name: "Carlos Vega",
+//       driver_photo: "https://cdn/img/driver12.jpg",
+//       desvio: false,
+//       coordinates: [
+//         { lat: -12.046, lng: -77.042, timestamp: "2025-11-10T12:00:00Z" },
+//         { lat: -12.047, lng: -77.041, timestamp: "2025-11-10T12:01:00Z" },
+//         { lat: -12.048, lng: -77.04, timestamp: "2025-11-10T12:02:00Z" },
+//         { lat: -12.049, lng: -77.039, timestamp: "2025-11-10T12:03:00Z" },
+//         { lat: -12.05, lng: -77.038, timestamp: "2025-11-10T12:04:00Z" },
+//       ],
+//       fecha: new Date().toISOString(),
+//     },
+//   ];
 
-  return NextResponse.json({ routes, driverId });
-}
+//   return NextResponse.json({ routes, driverId });
+// }

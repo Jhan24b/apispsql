7f139cde07a6b7e7552dd00113d25a7c43aba730
Jhan24b <jhan24b@gmail.com>
2025-11-11 22:14:41 -0500

    path v2

--- app/api/colective/path/[driverId]/route.ts
+++ app/api/colective/path/[driverId]/route.ts
@@ -1,7 +1,7 @@
 import pool from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
     const body = await req.json();
     const { driverId, desvio, coords } = body;
@@ -11,25 +11,25 @@ export async function POST(req: NextRequest) {
         { error: "Parámetros inválidos: falta driverId o coords[]" },
         { status: 400 }
       );
-    } else {
-      const result = await pool.query(
-        `INSERT INTO "BDproyect"."path" (driverId, desvio, coords)
-      VALUES ($1, $2, $3)
-      RETURNING *;
-      `,
-        [driverId, desvio, coords]
-      );
-      if (result.rows.length > 0) {
-        return NextResponse.json(
-          {
-            result: result.rows[0]
-          },
-          { status: 201 }
-        );
-      }
     }
+
+    const result = await pool.query(
+      `INSERT INTO "BDproyect"."path" (driverId, desvio, coords)
+       VALUES ($1, $2, $3)
+       RETURNING *;`,
+      [driverId, desvio, coords]
+    );
+
+    if (result.rows.length === 0) {
+      return NextResponse.json(
+        { error: "No se pudo insertar el registro" },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json({ result: result.rows[0] }, { status: 201 });
   } catch (err) {
-    console.log(err);
+    console.error(err);
     return NextResponse.json(
       { error: "Error al procesar la solicitud" },
       { status: 500 }
